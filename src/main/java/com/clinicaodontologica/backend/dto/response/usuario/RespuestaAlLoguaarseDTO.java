fe99cc1b5913277052b562bbb40bfcb84213c868
JoseIgnacioDav <joseignacio.dav@gmail.com>
2026-09-20 08:51:35 -0500

    creo el dto RespuestAlLoguearseDTO para pasar al front unicamente los datos necesarios al hacer login

---
+++ src/main/java/com/clinicaodontologica/backend/dto/response/usuario/RespuestaAlLoguaarseDTO.java
@@ -0,0 +1,65 @@
+package com.clinicaodontologica.backend.dto.response.usuario;
+
+public class RespuestaAlLoguaarseDTO {
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private String email;
+    private String cedula;
+    private String rol;
+
+    // constructor vacio
+
+    public RespuestaAlLoguaarseDTO(){}
+
+    // gettes y setters)
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
